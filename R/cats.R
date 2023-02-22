0f07ebd1a556198ae6f23d3bedec1de9e58a5ee2
Karl veggerby <karlveggerby@Karls-personal-macbookpro13.local>
2023-02-22 12:22:36 -0800

    updated package with description and licence

--- R/cats.R
+++ R/cats.R
@@ -1,3 +1,11 @@
+#' Expresses feelings about cats
+#'
+#' @param love A logical argument indicating if you love cats. Either TRUE or FALSE
+#'
+#' @return returns one of two possible character strings describing your feelings about cats
+#' @export
+#'
+#' @examples cats(TRUE)
 cats<-function(love=TRUE){
   if (love==TRUE) {
     msg<-"I love cats"
